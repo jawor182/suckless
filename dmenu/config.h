6f1b52694b8f6ee790700af58036138f1cfc0984
jawor <jakubjaworski@mailbox.org>
2025-11-21 22:21:05 +0100

    Modified dmenu's center position to actual center of the screen

--- dmenu/config.h
+++ dmenu/config.h
@@ -4,7 +4,7 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static int centered = 0;                    /* -c option; centers dmenu on screen */
 static int min_width = 800;                    /* minimum width when centered */
-static const float menu_height_ratio = 4.0f;  /* This is the ratio used in the original calculation */
+static const float menu_height_ratio = 2.0f;  /* This is the ratio used in the original calculation */
 
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static char font[] = "monospace:size=10";
