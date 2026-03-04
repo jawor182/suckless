23de04d62e74824111ab5b5d113f7331c8f50ae0
jawor <jakubjaworski@mailbox.org>
2026-03-04 21:16:37 +0100

    removed borderpx from st and aerc

--- st/config.h
+++ st/config.h
@@ -8,7 +8,7 @@
 static char *font = "JetBrainsMonoNerdFont=16:antialias=true:autohint=true";
 static char *font2[] = { "NotoColorEmoji:pixelsize=16:antialias=true:autohint=true" };
 
-static int borderpx = 2;
+static int borderpx = 0;
 
 /*
  * What program is execed by st depends of these precedence rules:
